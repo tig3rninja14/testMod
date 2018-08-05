5b56696253e6cc95fefd262f46e994636cfa7a17
Tig3r <Tig3r@DESKTOP-528V7E0>
2018-08-05 18:48:35 -0500

    Implemented update.json & dependency fixes

--- src/main/java/com/tiger/testMod/Main.java
+++ src/main/java/com/tiger/testMod/Main.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, dependencies = Reference.DEPENCIES, updateJSON = Reference.UPDATE_JSON)
 public class Main {
 	
 	@Instance
--- src/main/java/com/tiger/testMod/util/Reference.java
+++ src/main/java/com/tiger/testMod/util/Reference.java
@@ -5,8 +5,10 @@ public class Reference {
 	//reference vars
 	public static final String MOD_ID = "tm";
 	public static final String NAME = "Test Mod";
-	public static final String VERSION = "0.1";
+	public static final String VERSION = "0.1.1";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]"; //not sure why the brackets
+	public static final String DEPENCIES = "required-after:FML";
+	public static final String UPDATE_JSON = "https://github.com/tig3rninja14/testMod/blob/master/update.json";
 	public static final String CLIENT_PROXY_CLASS = "com.tiger.testMod.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.tiger.testMod.proxy.CommonProxy";
 	
