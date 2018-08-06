5e55e5a6a94e7b581dbbbb0f12ce7dd3123aaaec
Tig3r <Tig3r@DESKTOP-528V7E0>
2018-08-05 21:11:03 -0500

    Changed item dropped from ruby_ore to ruby

--- src/main/java/com/tiger/testMod/blocks/RubyOre.java
+++ src/main/java/com/tiger/testMod/blocks/RubyOre.java
@@ -1,9 +1,16 @@
 package com.tiger.testMod.blocks;
 
+import java.util.Random;
+
+import com.tiger.testMod.init.ModItems;
+
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
 
 public class RubyOre extends BlockBase{
+	
+	private static int numDrop = 3;
 
 	public RubyOre(String name, Material material) {
 		super(name, material);
@@ -14,5 +21,13 @@ public class RubyOre extends BlockBase{
 		setLightLevel(0.0F);
 		setLightOpacity(0);
 	}
+	
+	public Item getItemDropped(int metadata, Random rand, int fortune){
+		return ModItems.RUBY;
+		}
+	
+	public int quantityDropped(Random rand){
+		return numDrop;
+		}
 
 }
